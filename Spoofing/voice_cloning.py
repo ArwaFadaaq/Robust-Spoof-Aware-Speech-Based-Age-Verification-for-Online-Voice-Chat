91c51791e502f53fbfadd7e9cf9925c6f53ef822
kawthrbawzir <kawthrbw1@gmail.com>
2026-04-30 00:39:23 +0300

    Update voice_cloning.py

--- Spoofing/voice_cloning.py
+++ Spoofing/voice_cloning.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from Spoofing.koko_tts import run_koko_tts
-from Spoofing.f5_tts import run_f5_tts
-from Spoofing.chatterbox_tts import run_chatterbox_tts
-
+from koko_tts import run_koko_tts
+from f5_tts import run_f5_tts
+from chatterbox_tts import run_chatterbox_tts
 
 TTS_ENGINES = {
     "koko": run_koko_tts,
@@ -11,13 +10,11 @@ TTS_ENGINES = {
     "chatterbox": run_chatterbox_tts
 }
 
-
 def run_tts_on_file(
     text: str,
     reference_audio_path: str,
     model_name: str,
 ) -> str:
-
     return TTS_ENGINES[model_name](
         text=text,
         reference_audio_path=reference_audio_path,
