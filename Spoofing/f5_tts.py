cd134bfe76d9f50826d77ceda51522a03b8b8bae
kawthrbawzir <kawthrbw1@gmail.com>
2026-04-29 23:47:03 +0300

    Update f5_tts.py

--- Spoofing/f5_tts.py
+++ Spoofing/f5_tts.py
@@ -1,43 +1,36 @@
 # -*- coding: utf-8 -*-
- 
+
 import os
 import uuid
 import shutil
- 
+
 _CLIENT = None
- 
- 
+
+
 def get_model():
     global _CLIENT
     if _CLIENT is None:
         from gradio_client import Client
         _CLIENT = Client("mrfakename/E2-F5-TTS")
     return _CLIENT
- 
- 
+
+
 def run_f5_tts(
     ref_audio_path: str,
     gen_text: str,
     ref_text: str = "",
     remove_silence: bool = True,
 ) -> str:
- 
+
     from gradio_client import handle_file
-    from pydub import AudioSegment
- 
+
     client = get_model()
- 
+
     ref_wav = f"/content/{uuid.uuid4()}_ref.wav"
     out_path = f"/content/{uuid.uuid4()}_f5_tts.wav"
- 
-    if not ref_audio_path.endswith(".wav"):
-        AudioSegment.from_file(ref_audio_path)\
-            .set_frame_rate(24000)\
-            .set_channels(1)\
-            .export(ref_wav, format="wav")
-    else:
-        shutil.copy(ref_audio_path, ref_wav)
- 
+
+    shutil.copy(ref_audio_path, ref_wav)
+
     result = client.predict(
         ref_audio=handle_file(ref_wav),
         ref_text=ref_text,
@@ -45,11 +38,11 @@ def run_f5_tts(
         remove_silence=remove_silence,
         api_name="/predict"
     )
- 
+
     shutil.copy(result, out_path)
- 
+
     if os.path.exists(ref_wav):
         os.remove(ref_wav)
- 
+
     return out_path
  
