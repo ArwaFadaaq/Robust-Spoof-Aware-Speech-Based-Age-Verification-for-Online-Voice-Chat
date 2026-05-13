28cb7f6f26075ed5e3d5205a167230f6c30dc24c
kawthrbawzir <kawthrbw1@gmail.com>
2026-05-13 07:50:00 +0300

    Update text_to_speech.py

--- Spoofing/text_to_speech.py
+++ Spoofing/text_to_speech.py
@@ -3,11 +3,13 @@
 from koko_tts import run_koko_tts
 from f5_tts_local import run_f5_tts
 from chatterbox_tts import run_chatterbox_tts
+from coqui_tts import run_coqui_tts
 
 TTS_ENGINES = {
     "koko": run_koko_tts,
     "f5": run_f5_tts,
-    "chatterbox": run_chatterbox_tts
+    "chatterbox": run_chatterbox_tts,
+    "coqui": run_coqui_tts
 }
 
 def run_tts_on_file(
@@ -15,6 +17,7 @@ def run_tts_on_file(
     reference_audio_path: str,
     model_name: str,
 ) -> str:
+
     return TTS_ENGINES[model_name](
         text=text,
         reference_audio_path=reference_audio_path,
