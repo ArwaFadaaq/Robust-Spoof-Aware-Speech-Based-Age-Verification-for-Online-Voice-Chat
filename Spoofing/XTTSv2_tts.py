d55ce8246bffb4ac023e0f194270a6e80ed7950b
Sumiahzsaeed <sumiahsaeed1@gmail.com>
2026-05-14 08:11:57 +0300

    Update XTTSv2_tts.py

--- Spoofing/XTTSv2_tts.py
+++ Spoofing/XTTSv2_tts.py
@@ -1,4 +1,6 @@
 import uuid
+import librosa
+import numpy as np
 import torch
 from TTS.api import TTS
 
@@ -6,14 +8,23 @@ _MODEL = None
 
 def _load_model():
     global _MODEL
+
     if _MODEL is None:
-        _MODEL = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(
+        _MODEL = TTS(
+            "tts_models/multilingual/multi-dataset/xtts_v2"
+        ).to(
             "cuda" if torch.cuda.is_available() else "cpu"
         )
+
     return _MODEL
 
 
-def run_XTTSv2_tts(text: str, reference_audio_path: str) -> str:
+def run_XTTSv2_tts(
+    text: str,
+    reference_audio_path: str,
+    sr: int = 16000
+) -> np.ndarray:
+
     model = _load_model()
 
     out_path = f"/content/{uuid.uuid4()}_xtts.wav"
@@ -25,4 +36,6 @@ def run_XTTSv2_tts(text: str, reference_audio_path: str) -> str:
         file_path=out_path
     )
 
-    return out_path
+    audio, _ = librosa.load(out_path, sr=sr)
+
+    return audio.astype(np.float32)
